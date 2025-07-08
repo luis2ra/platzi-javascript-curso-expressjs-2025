72eb5f4e69f5083ed3d9ad257117ebab280dd1a7
Luis Altuve <luis2ra@gmail.com>
2025-07-08 11:40:26 -0500

    class02: settings to npm and nvm

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send(`
+    <h1>Welcome to the Express.js API v1</h1>
+    <p>This is a simple API built with Express.js.</p>
+    <p>Running on port: ${PORT}</p>
+`);
 });
 
 app.listen(PORT, () => {
