3fa093abc85416b7985915303f1d375382c739a8
Luis Altuve <luis2ra@gmail.com>
2025-07-08 13:01:11 -0500

    class04: installed body-parser package

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 require('dotenv').config(); // Load environment variables from .env file
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json()); // Middleware to parse JSON bodies
+app.use(bodyParser.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,6 +14,24 @@ app.get('/', (req, res) => {
     <p>This is a simple API built with Express.js.</p>
     <p>Running on port: ${PORT}</p>
 `);
+});
+
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  
+  res.send(`
+    <h1>User Details</h1>
+    <p>User ID: ${userId}</p>
+    `);
+});
+
+app.get('/search', (req, res) => {
+  const query = req.query.q;
+
+  res.send(`
+    <h1>Search Results</h1>
+    <p>Search Query: ${query}</p>
+  `);
 });
 
 app.listen(PORT, () => {
